caf64a0434bd7934a3a42f688e9a1c27e23b73b4
Ricardo Joaquinito <rjoaquinito@gmail.com>
2023-07-26 12:40:26 +0200

    Added exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
 
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
-
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filterText, setNewFilterText] = useState('')
@@ -28,7 +23,7 @@ const App = () => {
     const handleFilterTextChange = (event) => {
         setNewFilterText(event.target.value)
         
-        // Case insensity is achieved by putting both strings in lower case.
+        // Case insensivity is achieved by putting both strings in lower case.
         // Because of the asynchronous nature of the React state set function,
         // we cannot use the filterText variable yet.
         let filteredArray = persons.filter(
@@ -49,6 +44,18 @@ const App = () => {
                                     id: persons.length+1}))                          
         }
     }
+
+    const getPersonsFromServer = () => {
+      console.log('effect')
+      axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          console.log('promise fulfilled')
+          setPersons(response.data)
+        })
+    }
+    
+    useEffect(getPersonsFromServer, [])
 
     return (
         <div>
