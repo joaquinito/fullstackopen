b28ce3f09e3c632551316d3efa8150f13ccba78f
Ricardo Joaquinito <rjoaquinito@gmail.com>
2023-08-01 23:49:17 +0200

    Added exercise 2.19

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,54 +5,67 @@ import restCountries from "./services/restCountries"
 
 function App() {
 
-  /* State variables */
-  const [inputText, setInputText] = useState('')
-  const [countriesData, setCountriesData] = useState([])
-  const [searchResult, setSearchResult] = useState([])
+    /* State variables */
+    const [inputText, setInputText] = useState('')
+    const [countriesData, setCountriesData] = useState([])
+    const [searchResult, setSearchResult] = useState([])
 
-  /* Event handlers */
-  const handleInputChange = (event) => {
+    /* Event handlers */
+    const handleInputChange = (event) => {
 
-    setInputText(event.target.value)
+        setInputText(event.target.value)
 
-    if (event.target.value === '') {
-      setSearchResult([])
-      console.log("Empty input")
-    }
-
-    else{
-      let filteredCountries = countriesData.filter(
-        country => country.name.common.toLowerCase().includes(event.target.value.toLowerCase()))
-
-      // Check if there a full name match (to avoid the Sudan vs. South Sudan problem)
-      if (filteredCountries.length > 1 && filteredCountries.length <= 10) {
-        for (let i = 0; i < filteredCountries.length; i++) {
-          if (filteredCountries[i].name.common.toLowerCase() === event.target.value.toLowerCase()){
-            filteredCountries = [filteredCountries[i]]
-            console.log("Full name match found")
-            break
-          }
+        if (event.target.value === '') {
+            setSearchResult([])
+            console.log("Empty input")
         }
-      }
-      
-      setSearchResult(filteredCountries)
-      console.log(filteredCountries)
+
+        else{
+            let filteredCountries = countriesData.filter(
+                country => country.name.common.toLowerCase()
+                    .includes(event.target.value.toLowerCase()))
+
+            // Check if there a full name match (to avoid the Sudan vs. South Sudan problem)
+            if (filteredCountries.length > 1 && filteredCountries.length <= 10) {
+                for (let i = 0; i < filteredCountries.length; i++) {
+                    if (filteredCountries[i].name.common.toLowerCase() === 
+                            event.target.value.toLowerCase()){
+                        filteredCountries = [filteredCountries[i]]
+                        console.log("Full name match found")
+                        break
+                    }
+                }
+            }
+            
+            setSearchResult(filteredCountries)
+            console.log(filteredCountries)
+        }
     }
-  }
 
-  // When the component is first rendered, fetch the data from the server
-  useEffect(() => {
-    restCountries.getAll()
-      .then(response => setCountriesData(response.data))
-      .catch(() => console.log("Error fetching the countries data"))
-  }, [])
+    const handleShowButtonClick = (countryName) => {
+        console.log(`Button clicked for ${countryName}`)
+        for (let i = 0; i < searchResult.length; i++) {
+            if (searchResult[i].name.common === countryName){
+                setSearchResult([searchResult[i]])
+                break
+            }
+        }
+    }
 
-  return (
-    <div>
-      <FindCountryForm textInput={inputText} inputHandlerFunction={handleInputChange} />
-      <CountryData data={searchResult}/>
-    </div>
-  );
+    // When the component is first rendered, fetch the data from the server
+    useEffect(() => {
+        restCountries.getAll()
+        .then(response => setCountriesData(response.data))
+        .catch(() => console.log("Error fetching the countries data"))
+    }, [])
+
+    /* App render */
+    return (
+        <div>
+            <FindCountryForm textInput={inputText} inputHandlerFunction={handleInputChange}/>
+            <CountryData data={searchResult} showButtonHandlerFunction={handleShowButtonClick}/>
+        </div>
+    );
 }
 
-export default App;
+export default App
--- part2/countries/src/components/CountryData.js
+++ part2/countries/src/components/CountryData.js
@@ -1,16 +1,20 @@
+const CountryData = ({ data, showButtonHandlerFunction }) => {
 
-const CountryData = ({ data }) => {
-
+    // No input text / no countries found
     if (data === []) {
         return (
             <div></div>
         )
     }
+
+    // More than 10 matches found
     else if (data.length > 10) {
         return (
             <div>Too many matches, specify another filter</div>
         )
     }
+
+    // One match found - show the country data
     else if (data.length == 1) {
         return (
             <div>
@@ -19,21 +23,28 @@ const CountryData = ({ data }) => {
                 <div>area {data[0].area}</div>
                 <h4>languages:</h4>
                 <ul>
-                    {Object.values(data[0].languages).map(language => <li key={language}>{language}</li>)}
+                    {Object.values(data[0].languages).map(language =>
+                        <li key={language}>{language}</li>)}
                 </ul>
                 <img src={data[0].flags.png} alt="flag"></img>
 
             </div>
         )
     }
+
+    // 2-10 matches found - show the country names and a button to show the data
     else {
         return (
-            <div>
-                {data.map(country => <div key={country.name.common}>{country.name.common}</div>)}
-            </div>
+            (data.map(country =>
+            (<div key={country.name.common}>
+                {country.name.common} &nbsp;
+                <button onClick={() =>
+                    showButtonHandlerFunction(country.name.common)}>show</button>
+            </div>)))
         )
     }
 
 }
+
 
 export default CountryData
