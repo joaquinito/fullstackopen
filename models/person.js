30db86d80259e7ffea39efe0a7a247b732892a8b
Ricardo Joaquinito <rjoaquinito@gmail.com>
2023-08-10 22:11:04 +0200

    Added exercise 3.20

--- models/person.js
+++ models/person.js
@@ -21,7 +21,19 @@ const personSchema = new mongoose.Schema({
         minlength: 3,
         required: true,
     },
-    number: String,
+    number: {
+        type: String,
+        minlength: 8,
+        required: true,
+        validate: {
+            validator: function (v) {
+                // Validate that this is a phone number: 2 or 3 digits first, then a '-', 
+                // then only digits
+                return /^\d{2,3}-\d+$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    },
 })
 
 personSchema.set('toJSON', {
