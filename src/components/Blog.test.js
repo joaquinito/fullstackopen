542f805b4df012293e5e67024e00e321261a8efe
Ricardo Joaquinito <rjoaquinito@gmail.com>
2023-09-14 11:08:48 +0200

    Added exercise 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,26 +1,63 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('Default Blog content', () => {
+describe('Component \'Blog\': ', () => {
+
   const blog = {
     title: 'The test blog',
     author: 'Test Author',
     url: 'http://testurl.com',
-    likes: 0
+    likes: 0,
+    user: {
+      username: 'testUser',
+      name: 'Test User',
+      id: '000011112222333344445555'
+    },
   }
 
-  const dummyFunction = () => { }
+  const localStorageData = {
+    token: null,
+    username: 'testUser',
+    name: 'Test User',
+  }
 
-  const component = render(
-    <Blog blogData={blog}
-      incrementLikesHandler={dummyFunction}
-      removeBlogHandler={dummyFunction} />)
+  const mockFunction = jest.fn()
 
-  // Check if the component renders the title and author
-  screen.getByText('The test blog, by Test Author', { exact: false })
+  test('Default content is correct', () => {
 
-  // Check if the component does not render the url and likes
-  expect(component.container.querySelector('#blog-details')).toBeNull()
+    const component = render(
+      <Blog blogData={blog}
+        incrementLikesHandler={mockFunction}
+        removeBlogHandler={mockFunction} />)
+
+    // Check if the component renders the title and author
+    screen.getByText('The test blog, by Test Author', { exact: false })
+
+    // Check if the component does not render the url and likes
+    expect(component.container.querySelector('#blog-details')).toBeNull()
+  })
+
+  test('Clicking the button shows the url and likes', async () => {
+
+    const component = render(
+      <Blog blogData={blog}
+        incrementLikesHandler={mockFunction}
+        removeBlogHandler={mockFunction} />)
+
+    // Create a key-value pair in Local Storage to simulate that the test user is logged in
+    window.localStorage.setItem(
+      'loggedInAppUser', JSON.stringify(localStorageData))
+
+    // Start a user session to interact with the rendered component, and click the button
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    // Check if the component renders the url and likes after the button is clicked
+    screen.getByText('http://testurl.com', { exact: false })
+    screen.getByText(/likes+\s+\d/) // regex for 'likes' followed by one or more spaces and a digit
+  })
 })
