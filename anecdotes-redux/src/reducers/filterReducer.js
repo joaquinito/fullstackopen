6aa52f525f2a576db3875087476831eeb5b88049
Ricardo Joaquinito <rjoaquinito@gmail.com>
2023-09-21 10:07:47 +0200

    Added exercise 6.10

--- anecdotes-redux/src/reducers/filterReducer.js
+++ anecdotes-redux/src/reducers/filterReducer.js
@@ -1,26 +1,27 @@
-// Action creator for the 'SET_FILTER' action
-export const setFilter = (content) => {
-  return {
-    type: 'SET_FILTER',
-    payload: content
-  }
-}
+import { createSlice } from '@reduxjs/toolkit'
 
-const filterReducer = (state, action) => {
-  console.log('state: ', state)
+const initialState = ''
 
-  switch (action.type) {
+/*
+The createSlice function returns an object containing the reducer as well as the 
+action creators defined by the reducers parameter. The reducer can be accessed by 
+the noteSlice.reducer property, whereas the action creators by the noteSlice.actions property.
 
-    // Action 'SET_FILTER' will set the string that will be used to filter the anecdotes
-    case "SET_FILTER": {
-      console.log('action.payload', action.payload)
+The createSlice function's name parameter defines the prefix which is used in the action's 
+type values. For example, the createNote action defined later will have the type value of 
+filter/setFilter. The initialState parameter defines the reducer's initial state. The reducers 
+parameter takes the reducer itself as an object, of which functions handle state changes caused 
+by certain actions.
+*/
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setFilter(state, action) {
       return action.payload
     }
-
-    default: {
-      return ''
-    }
   }
-}
+})
 
-export default filterReducer
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
