fb00aa9285892f8f759dfd947c9cfa839fbc0221
Ricardo Joaquinito <rjoaquinito@gmail.com>
2023-09-26 10:36:52 +0200

    Added exercise 7.1

---
+++ anecdotes-routed/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+
+
+const Footer = () => {
+
+  const style = {
+    paddingTop: 20
+  }
+
+  return (
+    <div style={style}>
+      Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
+      Created by Ricardo Joaquinito.
+    </div>
+  )
+}
+
+export default Footer
